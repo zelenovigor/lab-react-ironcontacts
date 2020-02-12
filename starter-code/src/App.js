8fa599a531d0df5ccb983df6472685e2556d8bf3
Igor Zelenov <igorg.zelenov@gmail.com>
2020-02-12 14:08:08 -0500

    almost done

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -21,20 +21,74 @@ class App extends Component {
     return this.state.fiveContacts.map((eachContact, index) => {
       return (
         <tr key={index}>
-          <th><img src={eachContact.pictureUrl} alt={eachContact.name}/></th>
+          <th><img src={eachContact.pictureUrl} style={{height: '200px'}}alt={eachContact.name}/></th>
           <th>{eachContact.name}</th>
           <th>{eachContact.popularity}</th>
+          <td><button> onClick={() => this.deleteContact(index)}>Delete</button></td>
         </tr>
       )
   })
   }
+
+  // Iteration 2
+
+  addActor = () => {                                                          // add random actor
+    // Select a random number between 0 and 1
+    let randomNumber = Math.random();
+    // get the length of the contacts array
+    let lengthOfContacts = contacts.lengh;
+    // get the random element from array
+    let randomContact = Math.floor(randomNumber * lengthOfContacts);
+    // create a copy of the contacts array from state
+    let copyOfContactsFromState = [...this.state.contacts];
+    copyOfContactsFromState.push(contacts[randomContact]);
+    // set state with setState method (never set state directly)
+    this.setState({
+      contacts: copyOfContactsFromState
+    })
+  }                                                  
   
+  // Iteration 3
+
+  sortContactsByName = () => {                                                // sort actors by name
+    let contactsCopy = [...this.state.contacts];
+    contactsCopy.sort((a,b) => a.name.localeCompare(b.name));
+    this.setState({
+      contacts: contactsCopy
+    });
+  }
+
+  sortContactsByPopularity = () => {                                           // sort by popularity
+    let contactsCopy = [...this.state.contacts];
+    contactsCopy.sort((b,a) => a.popularity - b.popularity);
+    this.setState({
+      contacts: contactsCopy
+    });
+  }
+  
+  // Iteration 4 
+
+  deleteContact = (i) => {
+    let deletedContactArray = [...this.state.contacts];
+    deletedContactArray.splice(i, 1);
+    this.setState({
+      contacts: deletedContactArray
+    });
+  }
+
+
   render() {
     console.log("Render App.js")
     console.log(this.state.fiveContacts)
     return (
       <div className="App">
         <h1>Table</h1>
+        <button> onClick={() => this.addActor()}>Add Random Actor</button>
+        <button> onClick={() => this.addActor()}>Sort Actors By Name</button>
+        <button> onClick={() => this.addActor()}>Add Random Actor</button>                   
+                   
+
+
         <table>
           <thead>
             <tr>
